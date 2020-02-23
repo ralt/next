7b8d586a75952f6c6c45cd827220da59fa53eba1
Pierre Neidhardt <mail@ambrevar.xyz>
2020-02-23 14:01:35 +0100

    Fix ipc-window-make arguments.

--- source/window.lisp
+++ source/window.lisp
@@ -32,7 +32,7 @@
 
 (define-command make-window (&optional buffer)
   "Create a new window."
-  (let ((window (ipc-window-make *interface*))
+  (let ((window (ipc-window-make))
         (buffer (or buffer (make-buffer))))
     (window-set-active-buffer window buffer)
     (values window buffer)))
